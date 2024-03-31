81201966c8a621da644e23371fd80573100e9719
jadeyao0630 <jadeyao0630@gmail.com>
2024-03-31 21:41:49 +0800

    20240331

--- mssql_listener/Program.cs
+++ mssql_listener/Program.cs
@@ -5,73 +5,102 @@ using mssql_listener;
 
 using System.Timers;
 
-var parser = new IniFileParser();
-var iniData = parser.Parse("config.ini");
-var Mssql = "MsSqlServer";
-var Mysql = "MySqlServer";
-/*
-foreach (var section in iniData)
+
+class Program
 {
-    Console.WriteLine($"Section: {section.Key}");
-    foreach (var key in section.Value)
+    static void Main(string[] args)
     {
-        Console.WriteLine($"  {key.Key} = {key.Value}");
-    }
-}
-*/
-
-Console.WriteLine(iniData.ContainsKey(Mssql) && iniData.ContainsKey(Mysql));
-
-if (iniData.ContainsKey(Mssql) && iniData.ContainsKey(Mysql))
-{
-    var server = iniData[Mssql];
-    var server_mysql = iniData[Mysql];
-    if (server!=null && 
-        server.ContainsKey("Server") &&
-        server.ContainsKey("Database") &&
-        server.ContainsKey("User") &&
-        server.ContainsKey("Password") &&
-        server_mysql != null &&
-        server_mysql.ContainsKey("Server") &&
-        server_mysql.ContainsKey("Database") &&
-        server_mysql.ContainsKey("User") &&
-        server_mysql.ContainsKey("Password"))
-    {
-        var port = server.ContainsKey("Port") ? server["Port"] : "1433";
-        var port_mysql = server.ContainsKey("Port") ? server_mysql["Port"] : "3306";
-        //var data = $"server={server["Server"]},{port};Database={server["Database"]};User Id={server["User"]};Password={server["Password"]};Trusted_Connection=True";
-        DatabaseChangeNotification notifier = new DatabaseChangeNotification(new MsSqlServerData {
-            server=server["Server"],
-            port= int.Parse(port),
-            database = server["Database"],
-            user = server["User"],
-            password = server["Password"]
-
-        },new MySqlServerData
+        Console.WriteLine("参数数量: " + args.Length);
+        var isInit = false;
+        for (int i = 0; i < args.Length; i++)
         {
-            server = server_mysql["Server"],
-            port = int.Parse(port_mysql),
-            database = server_mysql["Database"],
-            user = server_mysql["User"],
-            password = server_mysql["Password"]
-        });
-        if (iniData.ContainsKey("General"))
-        {
-            var general = iniData["General"];
-            if (general.ContainsKey("tables") && !general["tables"].IsNullOrEmpty())
+            Console.WriteLine($"参数 {i + 1}: {args[i]}");
+            if(args[i]=="--init" || args[i] == "-i") isInit=true;
+            if (args[i]== "--help" || args[i] == "-h")
             {
-                notifier.StartListening(general["tables"].Split(','));
+                ShowHelp();
+                return;
             }
-            else
-            {
-                Console.WriteLine("tables have not been defineded...");
-            }
-            
         }
+        var parser = new IniFileParser();
+        var iniData = parser.Parse("config.ini");
+        var Mssql = "MsSqlServer";
+        var MssqlTarget = "MsSqlServerTarget";
 
+
+        if (iniData.ContainsKey(Mssql) && iniData.ContainsKey(MssqlTarget))
+        {
+            var server = iniData[Mssql];
+            var server_target = iniData[MssqlTarget];
+            if (server != null &&
+                server.ContainsKey("Server") &&
+                server.ContainsKey("Database") &&
+                server.ContainsKey("User") &&
+                server.ContainsKey("Password") &&
+                server_target != null &&
+                server_target.ContainsKey("Server") &&
+                server_target.ContainsKey("User") &&
+                server_target.ContainsKey("Password"))
+            {
+                var port = server.ContainsKey("Port") ? server["Port"] : "1433";
+                var port_target = server.ContainsKey("Port") ? server_target["Port"] : "1433";
+                //var data = $"server={server["Server"]},{port};Database={server["Database"]};User Id={server["User"]};Password={server["Password"]};Trusted_Connection=True";
+                mssqlChangeListerner listener = new mssqlChangeListerner(
+                    new database
+                    {
+                        server = server["Server"],
+                        port = int.Parse(port),
+                        databaseName = server["Database"],
+                        user = server["User"],
+                        password = server["Password"],
+                        isTrusted = server.ContainsKey("isTrusted") && server["isTrusted"] == "1",
+
+                    }, 
+                    new database
+                    {
+                        server = server_target["Server"],
+                        port = int.Parse(port_target),
+                        user = server_target["User"],
+                        password = server_target["Password"],
+                        isTrusted = server_target.ContainsKey("isTrusted") && server_target["isTrusted"] == "1",
+                    }
+                );
+
+                if (iniData.ContainsKey("General"))
+                {
+                    var general = iniData["General"];
+                    Console.WriteLine(general.ContainsKey("Tables"));
+                    if (general.ContainsKey("Tables") && !general["Tables"].IsNullOrEmpty())
+                    {
+                        listener.Start(general["Tables"].Split(','), isInit);
+                    }
+                    else
+                    {
+                        Console.WriteLine("tables have not been defineded...");
+                    }
+
+                }
+
+            }
+        }
+    }
+    static void ShowHelp()
+    {
+        // 帮助说明文本
+        string helpText = @"
+Usage: mssql_listener.exe [options]
+
+Options:
+  --help, -h      Show this help message and exit
+  --init, -i      Inital the target databse when start the application
+
+Description:
+  This is a C# console application designed to synchronize data from a source SQL Server to a target SQL Server.
+";
+
+        Console.WriteLine(helpText);
     }
 }
 
-//TrackingCT rrackingCT= new TrackingCT();
-//rrackingCT.run();
-//new ChangeTracker();
+
+
